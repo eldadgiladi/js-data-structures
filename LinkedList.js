ac2e9bc21953c2bfd9b5fa5baf59eb9cf68d091c
Eldad Giladi <eldadgiladi@gmail.com>
2018-09-23 19:26:14 +0300

    creating linkedList class

--- LinkedList.js
+++ LinkedList.js
@@ -3,43 +3,81 @@ const Node = require('./Node')
 const _head = new WeakMap()
 const _tail = new WeakMap()
 
-class LinkedList{
-    constructor(value){
+class LinkedList {
+    constructor(value) {
         const node = new Node(value)
-        _head.set(this,node)
-        _tail.set(this,node)
+        _head.set(this, node)
+        _tail.set(this, node)
     }
 
-    addToHead(val){
+    addToHead(val) {
         const oldHead = _head.get(this);
-        const newHead = new Node(val,oldHead);
+        const newHead = new Node(val, oldHead);
         oldHead.prev = newHead;
-        _head.set(this,newHead)
+        _head.set(this, newHead)
     }
 
-    addToTail(val){
+    addToTail(val) {
         const oldTail = _tail.get(this);
-        const newTail = newNode(val,null,oldTail);
+        const newTail = new Node(val, null, oldTail);
         oldTail.next = newTail;
-        _tail.set(this,newTail);
+        _tail.set(this, newTail);
     }
 
-    removeHead(){
+    removeHead() {
         const oldHead = _head.get(this);
-        const newHead = oldHead.next; 
-        if(this.length === 1) {
+        const newHead = oldHead.next;
+        if (this.length <= 1) {
             console.error('list must contain at least 1 argument')
             return new Error(`list must contain at least 1 argument`)
         }
-        
+        oldHead.prev = null;
+        _head.set(this, newHead);
+        return oldHead.value
     }
 
-    get length(){
+    removeTail() {
+        const oldTail = _tail.get(this);
+        const newTail = oldTail.prev;
+        if (this.length <= 1) {
+            console.error('list must contain at least 1 argument')
+            return new Error(`list must contain at least 1 argument`)
+        }
+        newTail.next = null;
+        _tail.set(this,newTail);
+        return oldTail.value;
+    }
+
+    get length() {
         let node = _head.get(this);
         let length = 1;
-        while(node.next !== null){
+        while (node.next !== null) {
             length++;
+            node = node.next
         }
         return length;
     }
+
+    toString(){
+        let node = _head.get(this);
+        let string = '';
+        while(node.next !== null){
+            string += node.value+', '
+            node = node.next;
+        }
+        string += node.value
+        return string;
+    }
+
+    search(val){
+        let node = _head.get(this)
+        while(node !== null){
+            if(node.value === val) return true;
+            node = node.next;
+        }
+        return false;
+    }
+
 }
+
+module.exports = LinkedList;
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const Node = require('./Node')
+const LinkedList = require('./LinkedList');
+
+const ll = new LinkedList(5)
+
+ll.addToHead(4)
+ll.addToTail(6)
+ll.addToHead(3)
+ll.addToTail(7)
+console.log(ll.search(6));
+console.log(ll.toString());
+ll.removeHead()
+ll.removeHead()
+ll.removeHead()
+ll.removeTail()
+ll.removeTail()
+console.log(ll.toString());
+console.log(ll.search(7));
+
+
 
